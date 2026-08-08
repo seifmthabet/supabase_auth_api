38bd7a7d0334c683162e0d18025c93bccfc84813
seifmthabet <seifmthabet@gmail.com>
2026-08-08 17:41:45 +0300

    Stage 0: setup server and supabase client

---
+++ src/config/supabase.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const supabase = createClient(
+    process.env.SUPABASE_URL as string,
+    process.env.SUPABASE_KEY as string, {
+        auth : {
+            autoRefreshToken: false,
+            persistSession: false,
+            detectSessionInUrl: false
+        }
+    }
+)
+
---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+
+import express , { type Request, type Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (req: Request, res: Response) => {
+    res.status(200).json({
+        status: "ok",
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
